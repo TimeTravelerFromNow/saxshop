584f046b913e91a1d3e4a8482ec746f10528c7eb
SebD <98575184+TimeTravelerFromNow@users.noreply.github.com>
2024-06-04 00:22:07 -0700

    adds production secrets

--- app/models/concerns/reserved_slugs.rb
+++ app/models/concerns/reserved_slugs.rb
@@ -1,3 +1,3 @@
 module ReservedSlugs
-  RESERVED_SLUGS = %w(teachers site site_settings flickr_photos categories instruments makes up icons)
+  RESERVED_SLUGS = %w(teachers dashboard site site_settings flickr_photos categories instruments makes up icons)
 end
--- app/models/flickr_photo.rb
+++ app/models/flickr_photo.rb
@@ -2,6 +2,8 @@ class FlickrPhoto < ApplicationRecord
   belongs_to :instrument, optional: true
   scope :instrumentless, -> { self.where(instrument: nil) }
   before_save :update_photo_set_count
+
+  validates_presence_of :embed
   # api
   def self.flickr
     if @flickr.nil?
@@ -21,11 +23,12 @@ class FlickrPhoto < ApplicationRecord
       Flickr::Photoset.new( self.embed, api_key).getPhotos
     rescue StandardError => e
       Rails.logger.info(e)
+      return nil
     end
   end
 
   def update_photo_set_count
-    self.photo_set_count = self.photo_set.count || 0
+    self.photo_set_count = (self.photo_set.count if self.photo_set ) || 0
   end
 
   def self.api_key_missing?
